e6cd638860ef823f266962ad51f5c601aed478c7
3kdeveloper <kamrankhan@techswivel.com>
2023-10-21 14:09:42 +0500

    push code with errors

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Materialpp(
       title: 'Flutter CI/CD',
       theme: ThemeData(
         // This is the theme of your application.
